7cd04eb446d835e6e0ce5d369e171afbdc45fd90
JJONK19 <josuejaviera19@gmail.com>
2022-11-27 01:26:32 -0600

    Comando rmgrp

---
+++ rmgrp.h
@@ -0,0 +1,11 @@
+#ifndef RMGRP_H
+#define RMGRP_H
+
+#include <iostream>
+#include <regex>
+
+#include "structs.h"
+
+void rmgrp(std::vector<std::string> &parametros, std::vector<disco> &discos, usuario &sesion);
+
+#endif
