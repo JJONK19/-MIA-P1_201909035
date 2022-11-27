53bf890e2be3f00d74e906d101c1ad5558514323
JJONK19 <josuejaviera19@gmail.com>
2022-11-27 01:25:56 -0600

    Comando chgrp

---
+++ chgrp.h
@@ -0,0 +1,11 @@
+#ifndef CHGRP_H
+#define CHGRP_H
+
+#include <iostream>
+#include <regex>
+
+#include "structs.h"
+
+void chgrp(std::vector<std::string> &parametros, std::vector<disco> &discos, usuario &sesion);
+
+#endif
